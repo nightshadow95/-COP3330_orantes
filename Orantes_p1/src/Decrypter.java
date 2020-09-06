041401e3d7dbcb9602509df1aa5b22e3aaa97cf0
Ricardo Orantes <ricardo.orantes@knights.ucf.edu>
2020-09-06 15:46:14 -0400

    Files for project 1 of this course

---
+++ Orantes_p1/src/Decrypter.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Decrypter {
+
+    public static String decrypt(String Message) {
+        return ConvertArray(Equations(ConvertString(Message)));
+    }
+
+    public static int [] ConvertString(String Decrypt) {
+        int [] number = new int[Decrypt.length()];
+
+        for(int i=0; i < Decrypt.length(); i++) {
+            number[i] =  Integer.parseInt(String.valueOf(Decrypt.charAt(i)));
+        }
+        return number;
+    }
+
+    public static int [] Equations(int FirstNumbers []) {
+
+        int [] FinalNumbers = new int[4];
+
+        FinalNumbers[0] = (FirstNumbers[2]+10) -7;
+        FinalNumbers[1] = (FirstNumbers[3]+10) -7;
+        FinalNumbers[2] = (FirstNumbers[0]+10) -7;
+        FinalNumbers[3] = (FirstNumbers[1]+10) -7;
+
+        for (int i=0; i< 4; i++ ){
+            if (FinalNumbers[i] >= 10) {
+                FinalNumbers[i] = FinalNumbers[i] - 10;
+            }
+        }
+
+        return FinalNumbers;
+    }
+
+    public static String ConvertArray(int FinalNumbers []) {
+        return Arrays.toString(FinalNumbers).replace("[","").replace("]","").replace(", ","");
+    }
+}
---
+++ Orantes_p1/src/Encrypter.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Encrypter {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Encrypter MyEncrypter = new Encrypter();
+        Decrypter MyDecrypter = new Decrypter();
+
+        System.out.println("Hello, and welcome to the Encryption program.");
+        System.out.println("Please enter the 4 digits that you would like to encrypt: ");
+        String Encryption = scan.nextLine();
+
+        System.out.println("Encrypts " + "\"" + Encryption + "\"" + " to " + "\"" + MyEncrypter.encrypt(Encryption) + "\"");
+
+        System.out.println("This program can also allow users to decrypt messages.");
+        System.out.println("If you would like to use this part of the program then enter the encrypted value or type \"exit\" to exit the program");
+        String Decryption = scan.nextLine();
+
+        if (Decryption.toLowerCase().equals("exit")) {
+            System.out.println("Thank you, have a good day.");
+        }
+
+        else {
+            System.out.println("Decrypts " + "\"" + Decryption + "\"" + " to " + "\"" + MyDecrypter.decrypt(Decryption) + "\"");
+        }
+    }
+
+    public static String encrypt(String Message) {
+        return ConvertArray(Equations(ConvertString(Message)));
+    }
+
+    public static int [] ConvertString(String Encrypt) {
+        int [] number = new int[Encrypt.length()];
+
+        for(int i=0; i < Encrypt.length(); i++) {
+            number[i] =  Integer.parseInt(String.valueOf(Encrypt.charAt(i)));
+        }
+        return number;
+    }
+
+    public static int [] Equations(int[] FirstNumbers) {
+
+        int [] FinalNumbers = new int[4];
+
+        FinalNumbers[0] = (FirstNumbers[2]+7) %10;
+        FinalNumbers[1] = (FirstNumbers[3]+7) %10;
+        FinalNumbers[2] = (FirstNumbers[0]+7) %10;
+        FinalNumbers[3] = (FirstNumbers[1]+7) %10;
+
+        return FinalNumbers;
+    }
+
+    public static String ConvertArray(int[] FinalNumbers) {
+        return Arrays.toString(FinalNumbers).replace("[","").replace("]","").replace(", ","");
+    }
+}
